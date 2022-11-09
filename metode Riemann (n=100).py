4ea65f90aac505a5a1485e22e950f97410ebc77d
refimy <114718347+refimy@users.noreply.github.com>
2022-11-09 20:36:25 +0700

    menambahkan file

---
+++ metode Riemann (n=100).py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def func(x):
+    return (x**2)*np.exp(-x)
+a = 1.0
+b = 10.0
+n = 100
+
+#reiman
+x = np.linspace(a,b,n)
+dx = (x[-1]-x[0])/(n-1)
+
+hasil = 0
+
+for i in range(n-1):
+    hasil += dx*func(x[i])
+
+xp =np.linspace(a,b)
+plt.plot(xp,func(xp))
+for i in range (n-1):
+    plt.bar(x[i],func(x[i]), align = 'edge',width=dx, color ='red', edgecolor='black')
+plt.show()
+print (hasil)
